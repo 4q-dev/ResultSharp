1cc1bae3f2fe4afe4af58a78a7f546b6b07b24eb
LightChimera <artemilin347@gmail.com>
2025-03-08 23:43:09 +0300

    Map + Condition

--- src/ResultSharp/Extensions/FunctionalExtensions/Async/MapAsyncExtensions.cs
+++ src/ResultSharp/Extensions/FunctionalExtensions/Async/MapAsyncExtensions.cs
@@ -25,5 +25,24 @@ namespace ResultSharp.Extensions.FunctionalExtensions.Async
                 false => Result<TNew>.Failure(r.Errors)
             };
         }
+
+        /// <summary>
+        /// Maps the result of a task to a new result using the specified mapping function.
+        /// </summary>
+        /// <typeparam name="TOld">The type of the original result value.</typeparam>
+        /// <typeparam name="TNew">The type of the new result value.</typeparam>
+        /// <param name="result">The task representing the original result.</param>
+        /// <param name="map">The asynchronous function to map the original result value to the new result value.</param>
+        /// <param name="configureAwait">Indicates whether to configure await.</param>
+        /// <returns>A task representing the new result of the operation.</returns>
+        public static async Task<Result<TNew>> MapAsync<TOld, TNew>(this Task<Result<TOld>> result, Func<TOld, Task<TNew>> map, bool configureAwait = true)
+        {
+            var r = await result.ConfigureAwait(configureAwait);
+            return r.IsSuccess switch
+            {
+                true => await map(r).ConfigureAwait(configureAwait),
+                false => Result<TNew>.Failure(r.Errors)
+            };
+        }
     }
 }
--- src/ResultSharp/Extensions/FunctionalExtensions/Async/OnConditionAsyncExtensions.cs
+++ src/ResultSharp/Extensions/FunctionalExtensions/Async/OnConditionAsyncExtensions.cs
@@ -9,6 +9,8 @@ namespace ResultSharp.Extensions.FunctionalExtensions.Async
     /// </summary>
     public static class OnConditionAsyncExtensions
     {
+        #region OnSuccessAsync
+
         /// <summary>
         /// Executes the specified action if the result is successful.
         /// </summary>
@@ -53,7 +55,7 @@ namespace ResultSharp.Extensions.FunctionalExtensions.Async
         {
             var r = await result.ConfigureAwait(configureAwait);
             if (r.IsSuccess)
-                action(r);
+                action(r.Value);
 
             return r;
         }
@@ -70,10 +72,44 @@ namespace ResultSharp.Extensions.FunctionalExtensions.Async
         {
             var r = await result.ConfigureAwait(configureAwait);
             if (r.IsSuccess)
-                await action(r).ConfigureAwait(configureAwait);
+                await action(r.Value).ConfigureAwait(configureAwait);
 
             return r;
         }
+
+        /// <summary>
+        /// Executes the specified asynchronous action if the result is successful.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result value.</typeparam>
+        /// <param name="result">The result to check.</param>
+        /// <param name="action">The asynchronous action to execute if the result is successful.</param>
+        /// <param name="configureAwait">Indicates whether to configure await.</param>
+        /// <returns>A task representing the original result of the operation.</returns>
+        public static async Task<Result<TResult>> OnSuccessAsync<TResult>(this Result<TResult> result, Func<TResult, Task> action, bool configureAwait = true)
+        {
+            if (result.IsSuccess)
+                await action(result.Value).ConfigureAwait(configureAwait);
+            return result;
+        }
+
+        /// <summary>
+        /// Executes the specified asynchronous action if the result is successful.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result value.</typeparam>
+        /// <param name="result">The result to check.</param>
+        /// <param name="action">The asynchronous action to execute if the result is successful.</param>
+        /// <param name="configureAwait">Indicates whether to configure await.</param>
+        /// <returns>A task representing the original result of the operation.</returns>
+        public static async Task<Result<TResult>> OnSuccessAsync<TResult>(this Result<TResult> result, Func<Task> action, bool configureAwait = true)
+        {
+            if (result.IsFailure)
+                await action().ConfigureAwait(configureAwait);
+            return result;
+        }
+
+        #endregion
+
+        #region OnFailureAsync
 
         /// <summary>
         /// Executes the specified action if the result is a failure.
@@ -140,5 +176,37 @@ namespace ResultSharp.Extensions.FunctionalExtensions.Async
 
             return r;
         }
+
+        /// <summary>
+        /// Executes the specified asynchronous action if the result is a failure.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result value.</typeparam>
+        /// <param name="result">The result to check.</param>
+        /// <param name="action">The asynchronous action to execute if the result is a failure.</param>
+        /// <param name="configureAwait">Indicates whether to configure await.</param>
+        /// <returns>A task representing the original result of the operation.</returns>
+        public static async Task<Result<TResult>> OnFailureAsync<TResult>(this Result<TResult> result, Func<ReadOnlyCollection<Error>, Task> action, bool configureAwait = true)
+        {
+            if (result.IsFailure)
+                await action(result).ConfigureAwait(configureAwait);
+            return result;
+        }
+
+        /// <summary>
+        /// Executes the specified asynchronous action if the result is a failure.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result value.</typeparam>
+        /// <param name="result">The result to check.</param>
+        /// <param name="action">The asynchronous action to execute if the result is a failure.</param>
+        /// <param name="configureAwait">Indicates whether to configure await.</param>
+        /// <returns>A task representing the original result of the operation.</returns>
+        public static async Task<Result<TResult>> OnFailureAsync<TResult>(this Result<TResult> result, Func<Task> action, bool configureAwait = true)
+        {
+            if (result.IsFailure)
+                await action().ConfigureAwait(configureAwait);
+            return result;
+        }
+
+        #endregion
     }
 }
